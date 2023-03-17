bc94c62bb60e536cce6c0db15658693cac02c253
alvina4444 <akosua.nyamekye9@gmail.com>
2023-03-17 08:09:41 -0700

    c program 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,14 @@
 */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
+
 	putchar('\n');
 	return (0);
 }
