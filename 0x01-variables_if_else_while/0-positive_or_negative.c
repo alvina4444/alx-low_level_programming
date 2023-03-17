2bd0ebd37948c04012b79658b807396281cc4b94
alvina4444 <akosua.nyamekye9@gmail.com>
2023-03-17 07:21:07 -0700

    c-program 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,6 +11,7 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
@@ -23,6 +24,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	return (0);
 
+	return (0);
 }
