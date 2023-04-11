a8580dffda475f16704f63d460365c42c6de9311
alvina4444 <akosua.nyamekye9@gmail.com>
2023-04-11 10:24:05 -0700

    c program B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,27 +9,15 @@
  **/
 char *create_array(unsigned int size, char c)
 {
-	/* Return NULL if size is zero */
-	if (size == 0)
-	{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	}
 
-	/* Allocate memory for the array */
-	char *array = malloc(size * sizeof(char));
-
-	/* If allocation fails, return NULL */
-	if (array == NULL)
-	{
-		return (NULL);
-	}
-
-	/* Initialize the array with the given character */
-	for (unsigned int i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
-
-	/* Return the pointer to the array */
-	return (array);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
+
