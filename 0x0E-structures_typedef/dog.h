9fbe606b56d64249116cadbe9481309fb2dd6651
alvina4444 <akosua.nyamekye9@gmail.com>
2023-04-17 13:48:25 -0700

    c program E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
